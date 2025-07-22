1c1f719da3ab8aa85baf4b41260a5259b15e45c3
jahirbnavaz <54374698+jahirbnavaz@users.noreply.github.com>
2025-07-22 21:08:52 +0530

    Update Dynamic to Static main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "azurerm_public_ip" "main" {
   name                = "${var.vm_name}-pip"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_network_interface" "main" {
